9720ddbb63f5c4eee6a83c13d4ee88fbc06e5d75
shamlin143 <shamlin@gvtc.com>
2020-06-22 16:30:24 -0500

    changing styles

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,11 +4,7 @@ import "./style.css";
 function Jumbotron({ children }) {
   return (
 
-    // <div
-    //   style={{ height: 200, clear: "both", textAlign: "center", color: "black" }}
-    //   className="jumbotron">
-    //   {children}
-    // </div>
+
       <div className="jumbotron jumbotron-fluid d-flex align-items-center">
           <div className="container text-center">
               {children}
